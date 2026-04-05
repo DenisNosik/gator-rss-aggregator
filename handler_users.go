591980957fe557f78ef159ef012525d01e17098a
DenisNosik <denisnosik96@gmail.com>
2026-04-05 17:56:26 +0100

    add/users handler

---
+++ handler_users.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/DenisNosik/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return errors.New("Login command expects a username")
+	}
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.Args[0],
+	})
+	if err != nil {
+		return err
+	}
+
+	if err := s.cfg.SetUser(user.Name); err != nil {
+		return err
+	}
+
+	s.cfg.CurrentUserName = cmd.Args[0]
+	fmt.Printf("User: %s was created.\n", user.Name)
+
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return errors.New("Login command expects a username")
+	}
+
+	user, err := s.db.GetUser(context.Background(), cmd.Args[0])
+	if err != nil {
+		return err
+	}
+
+	if err := s.cfg.SetUser(user.Name); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if err := s.db.Reset(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("user table was reset")
+
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %s\n", user.Name)
+		}
+	}
+	return nil
+}
