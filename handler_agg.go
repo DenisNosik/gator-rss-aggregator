bb3ff7eb67c142004d3b8c28eb994bda2dd5ef18
DenisNosik <denisnosik96@gmail.com>
2026-04-06 20:21:01 +0100

    add/browse, posts table

--- handler_agg.go
+++ handler_agg.go
@@ -2,17 +2,74 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"time"
+
+	"github.com/DenisNosik/gator/internal/database"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	url := "https://www.wagslane.dev/index.xml"
-	rssFeed, err := fetchFeed(context.Background(), url)
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.Args) != 1 {
+		return errors.New("Agg command expects collecting feeds time e.g(1m, 5m, 1h etc.)")
 	}
 
-	fmt.Printf("Feed: %+v\n", rssFeed)
+	timeBetweenReqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenReqs)
+	fmt.Println()
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	defer ticker.Stop()
+
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	feedToFetch, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return err
+	}
+
+	if err := s.db.MarkFeedFetched(context.Background(), feedToFetch.ID); err != nil {
+		return err
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feedToFetch.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, rssItem := range rssFeed.Channel.Item {
+		pubDate, err := time.Parse(time.RFC1123Z, rssItem.PubDate)
+		if err != nil {
+			pubDate, err = time.Parse(time.RFC1123, rssItem.PubDate)
+		}
+		_, err = s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+			Title:       rssItem.Title,
+			Url:         rssItem.Link,
+			Description: sql.NullString{String: rssItem.Description, Valid: rssItem.Description != ""},
+			PublishedAt: sql.NullTime{Time: pubDate, Valid: err == nil},
+			FeedID:      feedToFetch.ID,
+		})
+		if err != nil {
+			if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+				continue
+			}
+			return err
+		}
+	}
 
 	return nil
 }
---
+++ handler_browse.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/DenisNosik/gator/internal/database"
+)
+
+func handlerBrowse(s *state, cmd command, user database.User) error {
+	var limit int32 = 2
+	if len(cmd.Args) != 0 {
+		parsed, err := strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("invalid limit: %w", err)
+		}
+		limit = int32(parsed)
+	}
+
+	posts, err := s.db.GetPostsForUser(context.Background(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  limit,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, post := range posts {
+		if post.PublishedAt.Valid {
+			fmt.Printf("%s from %s\n", post.PublishedAt.Time.Format("Mon Jan 2"), post.FeedName)
+		}
+		fmt.Printf("* %s\n", post.Title)
+		if post.Description.Valid {
+			fmt.Printf("* %v\n", post.Description.String)
+		}
+		fmt.Printf("* Link: %s\n", post.Url)
+		fmt.Println("=====================================")
+	}
+
+	return nil
+}
